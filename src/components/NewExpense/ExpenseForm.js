b33c5e3b17dd3a7a19e12639512db395690b757c
josegonzah <jose.gonzalezh@udea.edu.co>
2021-10-24 22:47:39 -0500

    Project finished

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -25,7 +25,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       data: new Date(enteredDate)
     };
 
